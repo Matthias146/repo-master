a5f7332d94c567f3693a4c304c34663cc17b5925
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-04-04 09:34:56 +0200

    feat(core): implement supabase client service and environments

---
+++ src/app/core/db/supabase.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Supabase {
+  private supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(environment.supabase.url, environment.supabase.key);
+  }
+
+  get client(): SupabaseClient {
+    return this.supabase;
+  }
+}
