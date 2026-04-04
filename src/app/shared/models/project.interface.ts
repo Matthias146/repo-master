02e9e745b0f84f4e1b2dfe198bac176c135664ad
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-04-04 12:47:36 +0200

    feat(shared): define typescript models for project database schema

---
+++ src/app/shared/models/project.interface.ts
@@ -0,0 +1,14 @@
+export type BlockType = 'hero' | 'tech-stack' | 'setup' | 'custom';
+
+export interface ReadMeBlock {
+  id: string;
+  type: BlockType;
+  data: Record<string, any>;
+}
+
+export interface Project {
+  id: string;
+  title: string;
+  blocks: ReadMeBlock[];
+  created_at: string;
+}
