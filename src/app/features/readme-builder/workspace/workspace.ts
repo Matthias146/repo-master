d6b7554f0d8171756212a969864cb6a2ac6da8d3
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-04-06 16:34:24 +0200

    feat(workspace): implement drag and drop using angular cdk

--- src/app/features/readme-builder/workspace/workspace.ts
+++ src/app/features/readme-builder/workspace/workspace.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-workspace',
-  imports: [],
+  imports: [CdkDropList, CdkDrag, CdkDropListGroup],
   templateUrl: './workspace.html',
   styleUrl: './workspace.scss',
 })
 export class Workspace {
+  availableBlocks = [
+    'Projekt-Titel & Header',
+    'Beschreibung',
+    'Installation',
+    'Features',
+    'Nutzung (Usage)',
+    'Technologien',
+    'Lizenz',
+  ];
 
+  selectedBlocks: string[] = [];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
