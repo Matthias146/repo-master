6fad19011843f1f746e22798e4f9a29b8066801e
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-04-08 08:58:34 +0200

    refactor(workspace): split monolithic ui into smart-dumb sidebar, editor and preview components

---
+++ src/app/features/readme-builder/workspace/editor/editor.ts
@@ -0,0 +1,45 @@
+import { Component, inject } from '@angular/core';
+import { ReadmeBlock, WorkspaceService } from '../../state/workspace.service';
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-editor',
+  imports: [CdkDropList, CdkDrag, ReactiveFormsModule],
+  templateUrl: './editor.html',
+  styleUrl: './editor.scss',
+})
+export class Editor {
+  public builder = inject(WorkspaceService);
+
+  markdownControl = new FormControl<string>('', { nonNullable: true });
+  nameControl = new FormControl<string>('', { nonNullable: true });
+
+  constructor() {
+    this.markdownControl.valueChanges.pipe(takeUntilDestroyed()).subscribe((val) => {
+      const id = this.builder.activeBlockId();
+      if (id) this.builder.updateBlock(id, { markdown: val });
+    });
+
+    this.nameControl.valueChanges.pipe(takeUntilDestroyed()).subscribe((val) => {
+      const id = this.builder.activeBlockId();
+      if (id) this.builder.updateBlock(id, { name: val.trim() || 'Unbenannt' });
+    });
+  }
+
+  selectBlock(block: ReadmeBlock) {
+    this.builder.activeBlockId.set(block.id);
+    this.markdownControl.setValue(block.markdown, { emitEvent: false });
+    this.nameControl.setValue(block.name, { emitEvent: false });
+  }
+
+  removeBlock(blockId: string, event: Event) {
+    event.stopPropagation();
+    this.builder.removeBlock(blockId);
+    if (!this.builder.activeBlockId()) {
+      this.markdownControl.setValue('', { emitEvent: false });
+      this.nameControl.setValue('', { emitEvent: false });
+    }
+  }
+}
