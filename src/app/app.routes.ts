7f3343983c971dbd48a36304c1681e240e9638f7
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-04-08 12:29:30 +0200

    fix(workspace): change h-screen to h-full to prevent layout cutoff inside shell wrapper

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 import { Workspace } from './features/readme-builder/workspace/workspace';
+import { Shell } from './core/layout/shell/shell';
+import { GitCommit } from './features/git-commit/git-commit';
 
 export const routes: Routes = [
   {
     path: '',
-    component: Workspace,
-    title: 'RepoMaster | Workspace',
-  },
-  {
-    path: '**',
-    redirectTo: '',
+    component: Shell,
+    children: [
+      { path: '', redirectTo: 'readme', pathMatch: 'full' },
+      { path: 'readme', component: Workspace },
+      { path: 'commits', component: GitCommit },
+    ],
   },
 ];
