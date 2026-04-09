474ff7abbd0df05e716bb8ce1616f6d1dd5fb42f
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-04-09 09:22:49 +0200

    feat(scanner): integrate public github api to auto-generate readme blocks from package.json

--- src/app/core/layout/shell/shell.ts
+++ src/app/core/layout/shell/shell.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { GithubScannerService } from '../../services/github-scanner.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shell',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './shell.html',
   styleUrl: './shell.scss',
 })
-export class Shell {}
+export class Shell {
+  private scanner = inject(GithubScannerService);
+
+  repoControl = new FormControl('');
+
+  triggerScan() {
+    const value = this.repoControl.value;
+    if (value) {
+      this.scanner.scanRepository(value);
+      this.repoControl.reset();
+    }
+  }
+}
